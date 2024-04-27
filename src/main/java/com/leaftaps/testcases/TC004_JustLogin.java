329bacf0aafda9883f7273cca0c4c64fa9563f89
indubeulah <beulaha.indu@gmail.com>
2024-04-27 17:35:06 +0530

    Changes to StatusBranch

--- src/main/java/com/leaftaps/testcases/TC004_JustLogin.java
+++ src/main/java/com/leaftaps/testcases/TC004_JustLogin.java
@@ -14,6 +14,7 @@ public class TC004_JustLogin extends ProjectHooks{
 		authors="Lakshmi";
 		category ="Smoke";
 		excelFileName="Login";
+		// Test changes
 	}
 	
 	@Test(dataProvider = "fetchData")
